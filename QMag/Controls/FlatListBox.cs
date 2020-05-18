86f91a00dcd6b2ab3eb38b834eaa99cd4ba2e6e7
winmo <win.moi@hotmail.com>
2020-05-18 19:44:26 +0200

    Correction de petits bugs de scroll

--- QMag/Controls/FlatListBox.cs
+++ QMag/Controls/FlatListBox.cs
@@ -29,7 +29,7 @@ namespace Controls
 
             pictureBox.Image = _menuFerme;
 
-            panelCorps.AutoScroll = false;
+            // désactive les barres de scroll mais rend le panel scrollable
             panelCorps.HorizontalScroll.Enabled = false;
             panelCorps.HorizontalScroll.Visible = false;
             panelCorps.HorizontalScroll.Maximum = 0;
@@ -41,14 +41,15 @@ namespace Controls
 
             _elements = new List<FlatButton>();
 
+            Size = new Size(Width, _hauteurCase);
+            panelCorps.Size = new Size(Width, 0);
+
             flatButtonUp.Location = new Point(0,0);
             flatButtonDown.Location = new Point(0,0);
         }
 
         public void Add(string text, EventHandler click = null)
         {
-            _vitesse += _vitesseOrigine; // augmente la vitesse à chaque création de bouton pour que le temps d'ouverture/fermeture reste le même
-
             FlatButton flatButton = new FlatButton
             {
                 Height = _hauteurCase,
@@ -56,7 +57,6 @@ namespace Controls
                 BackColor = Theme.Back,
                 TextAlign = ContentAlignment.MiddleLeft,
                 Width = panelCorps.Width, // fit la largeur du bouton au panel
-                //Location = new Point(0, panelCorps.MaximumSize.Height),
                 Location = new Point(0, (_elements.Count + 1) * _hauteurCase),
                 AutoSize = true // agrandit le bouton pour afficher le texte si il est trop long
 			};
@@ -69,9 +69,11 @@ namespace Controls
                 flatButton.Click += click; // abonne la fonction de retour à l'event click du bouton
             flatButton.Click += Click; // abonne la fonction de cette classe, permettant d'ouvrir/fermer le menu
 
-            if (_elements.Count < _nbElementsMax)
+            if (_elements.Count < _nbElementsMax) // évite que le menu n'aille trop bas
             {
-	            panelCorps.MaximumSize = new Size(panelCorps.Width, panelCorps.MaximumSize.Height + _hauteurCase);
+	            _vitesse += _vitesseOrigine; // augmente la vitesse à chaque création de bouton pour que le temps d'ouverture/fermeture reste le même
+
+                panelCorps.MaximumSize = new Size(panelCorps.Width, panelCorps.MaximumSize.Height + _hauteurCase);
 	            MaximumSize = new Size(panelCorps.Width, MaximumSize.Height + _hauteurCase);
             }
 
