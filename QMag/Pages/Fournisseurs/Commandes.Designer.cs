c3ee70e169d52b9290863fdd0340a1545bc79f02
winmo <win.moi@hotmail.com>
2020-05-20 14:59:24 +0200

    Ajout page historique des commandes

--- QMag/Pages/Fournisseurs/Commandes.Designer.cs
+++ QMag/Pages/Fournisseurs/Commandes.Designer.cs
@@ -28,17 +28,78 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.panelTitre = new System.Windows.Forms.Panel();
+			this.flatLabelTitre = new QMag.Controls.FlatLabel();
+			this.panelCorps = new System.Windows.Forms.Panel();
+			this.flatDataGridView = new QMag.Controls.FlatDataGridView();
+			this.panelTitre.SuspendLayout();
+			this.panelCorps.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// CommandeListe
+			// panelTitre
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Name = "CommandeListe";
+			this.panelTitre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(150)))), ((int)(((byte)(245)))));
+			this.panelTitre.Controls.Add(this.flatLabelTitre);
+			this.panelTitre.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panelTitre.Location = new System.Drawing.Point(0, 0);
+			this.panelTitre.MaximumSize = new System.Drawing.Size(0, 220);
+			this.panelTitre.Name = "panelTitre";
+			this.panelTitre.Size = new System.Drawing.Size(1039, 220);
+			this.panelTitre.TabIndex = 12;
+			// 
+			// flatLabelTitre
+			// 
+			this.flatLabelTitre.Anchor = System.Windows.Forms.AnchorStyles.Left;
+			this.flatLabelTitre.AutoSize = true;
+			this.flatLabelTitre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.flatLabelTitre.Font = new System.Drawing.Font("Yu Gothic UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.flatLabelTitre.ForeColor = System.Drawing.Color.White;
+			this.flatLabelTitre.Location = new System.Drawing.Point(52, 99);
+			this.flatLabelTitre.Name = "flatLabelTitre";
+			this.flatLabelTitre.Size = new System.Drawing.Size(350, 37);
+			this.flatLabelTitre.TabIndex = 0;
+			this.flatLabelTitre.Text = "Historique des commandes";
+			this.flatLabelTitre.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// panelCorps
+			// 
+			this.panelCorps.Controls.Add(this.flatDataGridView);
+			this.panelCorps.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panelCorps.Location = new System.Drawing.Point(0, 220);
+			this.panelCorps.Name = "panelCorps";
+			this.panelCorps.Size = new System.Drawing.Size(1039, 673);
+			this.panelCorps.TabIndex = 13;
+			// 
+			// flatDataGridView
+			// 
+			this.flatDataGridView.BackColor = System.Drawing.Color.White;
+			this.flatDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flatDataGridView.Location = new System.Drawing.Point(0, 0);
+			this.flatDataGridView.Name = "flatDataGridView";
+			this.flatDataGridView.Size = new System.Drawing.Size(1039, 673);
+			this.flatDataGridView.TabIndex = 0;
+			// 
+			// Commandes
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.Controls.Add(this.panelCorps);
+			this.Controls.Add(this.panelTitre);
+			this.Name = "Commandes";
+			this.Size = new System.Drawing.Size(1039, 893);
+			this.Load += new System.EventHandler(this.Commandes_Load);
+			this.panelTitre.ResumeLayout(false);
+			this.panelTitre.PerformLayout();
+			this.panelCorps.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Panel panelTitre;
+		private Controls.FlatLabel flatLabelTitre;
+		private System.Windows.Forms.Panel panelCorps;
+		private Controls.FlatDataGridView flatDataGridView;
 	}
 }
--- QMag/Pages/Fournisseurs/Commandes.cs
+++ QMag/Pages/Fournisseurs/Commandes.cs
@@ -1,12 +1,57 @@
-﻿using QMag.Core.Pages;
+﻿using System;
+using System.Collections.Generic;
+using Projet_magasin.Classes;
+using Projet_magasin.Gestion;
+using QMag.Core;
+using QMag.Core.Pages;
 
 namespace QMag.Pages.Fournisseurs
 {
 	public partial class Commandes : BaseConsulter
 	{
+		private readonly List<C_CommandesFournisseurs> _commandes;
+
 		public Commandes()
 		{
 			InitializeComponent();
+
+			_commandes = new G_CommandesFournisseurs(Connexion).Lire("id");
+		}
+
+		private void Commandes_Load(object sender, EventArgs e)
+		{
+			_flatDataGridView = flatDataGridView;
+
+			SetColonnes("Fournisseur", "Montant", "Date");
+
+			RempliColonnes();
+
+			AfterLoad();
+		}
+
+		private void RempliColonnes()
+		{
+			string fournisseur;
+			Money montant = new Money();
+			DateTime date;
+			foreach (C_CommandesFournisseurs commande in _commandes)
+			{
+				fournisseur = new G_Fournisseurs(Connexion).Lire_ID(commande.id_fournisseur).nom;
+				date = commande.date;
+
+				foreach (C_DetailAchat detailCommande in new G_DetailAchat(Connexion).Lire("id"))
+					if (detailCommande.id_commande == commande.id)
+						montant.Montant += detailCommande.prix * detailCommande.quantite;
+
+				_useGridView.Add(
+					fournisseur,
+					montant.Montant,
+					date
+				);
+
+				montant.Montant = 0;
+			}
+				
 		}
 	}
 }
